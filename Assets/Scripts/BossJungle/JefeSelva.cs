e5ac46302b3586bade18039e38453e9edd4dde80
HunterKiller989 <jeanpierrevivar989@hotmail.com>
2023-11-09 23:07:32 -0500

    IdlesJefe4

--- Assets/Scripts/BossJungle/JefeSelva.cs
+++ Assets/Scripts/BossJungle/JefeSelva.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class JefeSelva : MonoBehaviour
 {
@@ -8,9 +9,12 @@ public class JefeSelva : MonoBehaviour
     [SerializeField] private float hpEnemy;
     [SerializeField] private float normalSpeed; // Velocidad predeterminada de mi enemigo
     [SerializeField] private float increasedSpeed; // Uso esta velocidad para medir a cuanta velocidad va regresar a la lanza
+    private ChangeAnimation changeDirections;
+    private Animator anim;
     private float currentSpeed;
     private Rigidbody2D rb;
     private Transform player;
+    private BoxCollider2D player1;
 
     [Header("AtaqueLanza")]
     private Transform lanza;
@@ -24,9 +28,13 @@ public class JefeSelva : MonoBehaviour
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindWithTag("Player").transform;
         lanza = transform.Find("Lanza");
+
+        player1 = GameObject.FindWithTag("Player").GetComponent<BoxCollider2D>();
+        changeDirections = GetComponent<ChangeAnimation>();
 
         jefeSelva = GameObject.FindWithTag("JefeSelva").transform;
 
@@ -37,6 +45,9 @@ public class JefeSelva : MonoBehaviour
     {
         if (isFollowingPlayer)
         {
+            //Vector2 moveDirection = (player.position - transform.position).normalized;
+
+            //changeDirections.changeAnim(moveDirection);
             // Primero en esta etapa, mi enemigo sigue a mi jugador con la velocidad normal que yo le he dado
             transform.position = Vector2.MoveTowards(transform.position, player.position, currentSpeed * Time.deltaTime);
         }
@@ -48,6 +59,7 @@ public class JefeSelva : MonoBehaviour
             // Cuando el enemigo llega a la posición de la lanza, vincula la lanza a JefeSelva (Nombre con el que tengo a mi enemigo y tageado tmb), y cambia la velocidad a la que esta predeterminada, no a la acelerada
             if (Vector2.Distance(transform.position, lanzaPosition) < 0.1f)
             {
+                GetComponent<SpriteRenderer>().color = Color.white;
                 isFollowingPlayer = true;
                 Debug.Log("SeguirPlayer");
                 lanza.parent = jefeSelva;
@@ -61,6 +73,7 @@ public class JefeSelva : MonoBehaviour
         if (!isLanzaDetached && Time.time >= detachTime)
         {
             // En este punto tras haber hecho mi condicion quiero que cuando pasen los primeros 5 segundos, la lanza se va a desvincular de mi enemigo
+            GetComponent<SpriteRenderer>().color = Color.red;
             lanza.parent = null;
             isLanzaDetached = true;
             lanzaPosition = lanza.position;
