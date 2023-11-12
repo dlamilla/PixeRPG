89f9c3dde71143998ecf2cf65941c63f2bceda61
HunterKiller989 <jeanpierrevivar989@hotmail.com>
2023-11-12 06:48:56 -0500

    JefeDatosIncompletos

--- Assets/Scripts/BossJungle/JefeSelva.cs
+++ Assets/Scripts/BossJungle/JefeSelva.cs
@@ -7,14 +7,16 @@ public class JefeSelva : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float hpEnemy;
-    [SerializeField] private float normalSpeed; // Velocidad predeterminada de mi enemigo
-    [SerializeField] private float increasedSpeed; // Uso esta velocidad para medir a cuanta velocidad va regresar a la lanza
+    [SerializeField] private float normalSpeed;
+    [SerializeField] private float increasedSpeed;
     private ChangeAnimation changeDirections;
     private Animator anim;
     private float currentSpeed;
     private Rigidbody2D rb;
     private Transform player;
     private BoxCollider2D player1;
+    private bool hasLanzaGoAnimationPlayed = false;
+    private bool isLanzaGoResetting = false;
 
     [Header("AtaqueLanza")]
     private Transform lanza;
@@ -24,7 +26,16 @@ public class JefeSelva : MonoBehaviour
     private float returnToLanzaTime = 5.0f;
     private float detachStartTime = -1f;
     private Vector2 lanzaPosition;
-    private Transform jefeSelva; 
+    private Transform jefeSelva;
+
+    public enum TipoAtaque
+    {
+        LanzaAttack,
+        SaltoAttack
+    }
+
+    [SerializeField]
+    private TipoAtaque listaAtaque;
 
     void Start()
     {
@@ -32,16 +43,44 @@ public class JefeSelva : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindWithTag("Player").transform;
         lanza = transform.Find("Lanza");
-
         player1 = GameObject.FindWithTag("Player").GetComponent<BoxCollider2D>();
         changeDirections = GetComponent<ChangeAnimation>();
-
         jefeSelva = GameObject.FindWithTag("JefeSelva").transform;
-
         currentSpeed = normalSpeed;
+        listaAtaque = TipoAtaque.LanzaAttack;
     }
 
     void Update()
+    {
+        switch (listaAtaque)
+        {
+            case TipoAtaque.LanzaAttack:
+                UpdateLanzaAttack();
+            break;
+
+            case TipoAtaque.SaltoAttack:
+
+            break;
+
+        }
+    }
+
+    void LateUpdate()
+    {
+        switch (listaAtaque)
+        {
+            case TipoAtaque.LanzaAttack:
+                LateUpdateLanzaAttack();
+            break;
+
+            case TipoAtaque.SaltoAttack:
+
+            break;
+
+        }
+    }
+
+    private void UpdateLanzaAttack()
     {
         if (isFollowingPlayer)
         {
@@ -50,15 +89,12 @@ public class JefeSelva : MonoBehaviour
             changeDirections.changeAnim(moveDirection);
             anim.SetBool("enCamino", true);
 
-            // Primero en esta etapa, mi enemigo sigue a mi jugador con la velocidad normal que yo le he dado
             transform.position = Vector2.MoveTowards(transform.position, player.position, currentSpeed * Time.deltaTime);
         }
         else if (isLanzaDetached)
         {
-            // Segundo, hago llamar a esta linea de codigo para que mi enemigo regrese a la lanza tras los segundos 5 segundos  
             transform.position = Vector2.MoveTowards(transform.position, lanzaPosition, increasedSpeed * Time.deltaTime);
 
-            // Cuando el enemigo llega a la posición de la lanza, vincula la lanza a JefeSelva (Nombre con el que tengo a mi enemigo y tageado tmb), y cambia la velocidad a la que esta predeterminada, no a la acelerada
             if (Vector2.Distance(transform.position, lanzaPosition) < 0.1f)
             {
                 GetComponent<SpriteRenderer>().color = Color.white;
@@ -66,32 +102,55 @@ public class JefeSelva : MonoBehaviour
                 Debug.Log("SeguirPlayer");
                 lanza.parent = jefeSelva;
                 currentSpeed = normalSpeed;
+                listaAtaque= TipoAtaque.LanzaAttack;
             }
         }
     }
 
-    void LateUpdate()
+    private void LateUpdateLanzaAttack()
     {
         if (!isLanzaDetached && Time.time >= detachTime)
         {
-            // En este punto tras haber hecho mi condicion quiero que cuando pasen los primeros 5 segundos, la lanza se va a desvincular de mi enemigo
+            if (!hasLanzaGoAnimationPlayed)
+            {
+                anim.SetBool("lanzaGo", true);
+                hasLanzaGoAnimationPlayed = true;
+                StartCoroutine(ResetLanzaGoAfterDelay());
+            }
+
             GetComponent<SpriteRenderer>().color = Color.red;
             lanza.parent = null;
             isLanzaDetached = true;
             lanzaPosition = lanza.position;
             detachStartTime = Time.time;
-            // Cambia la velocidad a la velocidad aumentada que le he dado para cuando se desvincula de la lanza
             currentSpeed = increasedSpeed;
+            listaAtaque= TipoAtaque.LanzaAttack;
         }
         else if (isLanzaDetached && isFollowingPlayer && Time.time >= detachStartTime + returnToLanzaTime)
         {
             isFollowingPlayer = false;
         }
 
-        // Llamo a esta condicion solo cuando mi lanza se haya vinculado a mi enemigo (Osea que se haga hijo de mi enemigo xd), si eso ocurre, que el enemigo vuelva a seguir a mi jugador 
         if (lanza.parent == jefeSelva)
         {
             isFollowingPlayer = true;
+            listaAtaque= TipoAtaque.LanzaAttack;
         }
+    }
+
+    private IEnumerator ResetLanzaGoAfterDelay()
+    {
+        yield return new WaitForSeconds(0.85f);
+
+        if (!isLanzaGoResetting)
+        {
+            anim.SetBool("lanzaGo", false);
+            hasLanzaGoAnimationPlayed = false;
+        }
+    }
+
+    public void CancelResetLanzaGo()
+    {
+        isLanzaGoResetting = true;
     }
 }
