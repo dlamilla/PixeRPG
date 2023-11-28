9f918d5edd42d8c62678296404ace3a33cf3306f
DLamilla20 <lamilla.tenazoa@gmail.com>
2023-11-28 15:26:07 -0500

    FixHUD

---
+++ Assets/Scripts/Menu/CursorNoVisible.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorNoVisible : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    static void HideCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
