a1f92187ec6e86f3311af59c0664e28c95d702ac
DLamilla20 <lamilla.tenazoa@gmail.com>
2024-01-26 22:50:52 -0500

    HubBar

--- Assets/Scripts/Utilities/HealthBar.cs
+++ Assets/Scripts/Utilities/HealthBar.cs
@@ -6,9 +6,17 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image barImage;
+    [SerializeField] private Image barImageYellow;
 
     public void UpdateHealthBar(float maxHealth, float health)
     {
         barImage.fillAmount = health / maxHealth;
+        StartCoroutine(ChangeHit(maxHealth, health));
+    }
+
+    IEnumerator ChangeHit(float maxHealth1, float health1)
+    {
+        yield return new WaitForSeconds(0.3f);
+        barImageYellow.fillAmount = health1 / maxHealth1;
     }
 }
